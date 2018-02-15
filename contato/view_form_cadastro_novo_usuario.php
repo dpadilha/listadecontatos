a50394041ae90ce9c4660816525f677960025980
viniciusoliveira11 <viniciuswp11@gmail.com>
2018-02-15 20:24:15 -0300

    joao

--- contato/view_form_cadastro_novo_usuario.php
+++ contato/view_form_cadastro_novo_usuario.php
@@ -6,6 +6,8 @@
     <title>Cadastro de Contato</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="plugins/jQuery-Mask-Plugin/dist/jquery.mask.min.js"></script>
 
   	
 </head>
@@ -31,17 +33,22 @@
 
 		<div class="col-md-10">
 			<form method="POST" action="index.php?r=contato&p=cadastrar">
-			
-				<div class="form-group " >
-					<label >Digite o nome completo:</label>
-					<input class = "form-control" type="text" maxlength="120" name="txtNomeUsuario" >
+				<div class="row">
+					<div class="col-md-12">
+						<div class="form-group" >
+							<label >Digite o nome completo:</label>
+							<input class = "form-control" type="text" maxlength="120" name="txtNomeUsuario" required>
+						</div>
+					</div>
+					
+					<div class="col-md-12">
+						<div class="form-group">
+							<label>Informe a rua:</label>
+							<input class="form-control" type="text" maxlength="120" name="txtLograUsuario">
+						</div>
+					</div>
 				</div>
-
-				<div class="form-group ">
-					<label>Informe a rua:</label>
-					<input class="form-control" type="text" maxlength="120" name="txtLograUsuario">
-				</div>
-				<div class="container-fluid">
+				<div class="row">
 					<div class="col-md-6">
 						<div class="form-group">
 							<label>Informe o numero:</label>
@@ -52,9 +59,9 @@
 							<input class="form-control" type="text" maxlength="120" name="txtBairroUsuario">
 						</div>
 
-						<div class="form-group col-md-10">
+						<div class="form-group">
 							<label>Informe o telefone:</label>
-							<input class="form-control" type="text" maxlength="120" name="txtTelUsuario" placeholder="(xx) xxxxxxxx">
+							<input class="form-control" type="text" maxlength="120" name="txtTelUsuario" id="telefone" placeholder="(xx) xxxxxxxx">
 						</div>
 						<div class="form-group">
 							<label>Informe a cidade:</label>
@@ -72,14 +79,14 @@
 
 						<div class="form-group ">
 							<label>Informe o email:</label>
-							<input class="form-control" type="text" maxlength="120" name="txtEmailUsuario">
+							<input class="form-control" type="email" maxlength="120" name="txtEmailUsuario" required>
 						</div>
 							<div class="form-group ">
 							<label>Informe a data de nascimento:</label>
 							<input class="form-control" type="date" maxlength="120" name="txtDtnascUsuario">
 						</div>
 						
-						<div class="form-group col-md-10">
+						<div class="form-group">
 							<label>Informe o género sexual:</label><br/>
 							<input type="radio" name='txtSexoUsuario' value="masculino"> Masculino<br/>
 							<input type="radio" name='txtSexoUsuario' value="feminino"> Feminino
@@ -88,7 +95,7 @@
 					
 				</div>
 
-				<div class="form-group col-md-10">
+				<div class="form-group">
 					<input class="btn btn-primary" type="submit" value="Cadastrar Usuário">
 					<input type="hidden" name="frmCadUsuario">
 				</div>
@@ -100,5 +107,6 @@
 	</div>
 	
 </div>
+
 </body>
 </html>
